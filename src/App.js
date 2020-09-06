bf286af772b1a1d0bc81e10ce62e88efd1681c4c
Michael Harms <michael.harms6010@gmail.com>
2020-09-06 13:32:07 -0500

    ugly ugly proof of concept

--- src/App.js
+++ src/App.js
@@ -2,23 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [form, setForm] = React.useState({deckString: "", hashCount: 0})
+  const [result, setResult] = React.useState("")
+  const [calculating, setCalculating] = React.useState(false)
+
+  const handleChange = e => setForm({...form, [e.target.name]: e.target.value})
+
+  const calculate = _ => {
+    if (!calculating) {
+      setCalculating(true)
+    
+      let hash = form.deckString;
+      console.log(hash)
+      import('blake3/browser').then(blake3 => {
+        
+        for (let i = 0; i < +form.hashCount; i++) {
+          hash = blake3.hash(hash);
+        }
+          console.log(+form.hashCount)
+          setResult(hash.toString("hex"))
+          setCalculating(false)
+      })
+  }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <textarea
+        name="deckString"
+        value={form.deckString}
+        onChange={handleChange}/>
+
+      <input
+        name="hashCount"
+        value={form.hashCount}
+        onChange={handleChange}/>
+
+      <button type="button" onClick={calculate}>Calculate</button>
+      <p>Expected Proof Hash:</p>
+      {result && <p>{result}</p>}
     </div>
   );
 }
